49971f8a209e8512af49ea0aa3307c80bb63c876
Reijo Sund <reijo.sund@uef.fi>
2022-03-04 16:47:10 +0200

    Clear up extra folder

--- duckdb/src/duckdb/src/execution/operator/helper/physical_execute.cpp
+++
@@ -1,9 +0,0 @@
-#include "duckdb/execution/operator/helper/physical_execute.hpp"
-
-namespace duckdb {
-
-PhysicalExecute::PhysicalExecute(PhysicalOperator *plan)
-    : PhysicalOperator(PhysicalOperatorType::EXECUTE, plan->types, -1), plan(plan) {
-}
-
-} // namespace duckdb
--- duckdb/src/duckdb/src/storage/storage_info.cpp
+++
@@ -1,7 +0,0 @@
-#include "duckdb/storage/storage_info.hpp"
-
-namespace duckdb {
-
-const uint64_t VERSION_NUMBER = 31;
-
-} // namespace duckdb
